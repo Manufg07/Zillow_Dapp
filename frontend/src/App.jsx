23df429e99127d1706dab881165eb733a0e0512e
Manufg07 <manunarayananfg@gmail.com>
2024-09-25 10:16:37 +0530

    updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import PropertyList from "./components/PropertyList";
+import AddProperty from "./components/AddProperty"; 
 
 const App = () => {
   const [connectedAddress, setConnectedAddress] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false); 
 
-  
   const connectWallet = async () => {
     if (!window.ethereum) {
       alert("MetaMask is required to perform this action.");
@@ -23,26 +24,47 @@ const App = () => {
     }
   };
 
+  const openModal = () => {
+    setIsModalOpen(true); 
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false); 
+  };
+
   return (
     <div className="min-h-screen bg-gray-100 p-8">
       <header className="flex justify-between items-center mb-8">
         <h1 className="text-4xl font-bold text-gray-900">
           Real Estate Marketplace
         </h1>
-        <button
-          onClick={connectWallet}
-          className="bg-[#0D9488] text-white font-bold py-2 px-4 rounded-lg hover:bg-[#0F766E] transition-colors"
-        >
-          {connectedAddress
-            ? `Connected: ${connectedAddress.slice(
-                0,
-                6
-              )}...${connectedAddress.slice(-4)}`
-            : "Connect Wallet"}
-        </button>
+        <div>
+          <button
+            onClick={connectWallet}
+            className="bg-[#0D9488] text-white font-bold py-2 px-4 rounded-lg hover:bg-[#0F766E] transition-colors mr-4"
+          >
+            {connectedAddress
+              ? `Connected: ${connectedAddress.slice(
+                  0,
+                  6
+                )}...${connectedAddress.slice(-4)}`
+              : "Connect Wallet"}
+          </button>
+         
+          <button
+            onClick={openModal}
+            className="bg-blue-500 text-white font-bold py-2 px-4 rounded-lg hover:bg-blue-700 transition-colors"
+          >
+            Add Property
+          </button>
+        </div>
       </header>
 
+    
       <PropertyList connectedAddress={connectedAddress} />
+
+      
+      {isModalOpen && <AddProperty closeModal={closeModal} />}
     </div>
   );
 };
